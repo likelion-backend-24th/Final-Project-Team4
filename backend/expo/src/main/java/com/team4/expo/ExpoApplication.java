721d80662e60db412ef2a4aed1d39e7579c10b4a
donggeon <upzza8@gmail.com>
2026-09-01 15:48:26 +0900

    chore: MSA 백엔드 기본 구성

---
+++ backend/expo/src/main/java/com/team4/expo/ExpoApplication.java
@@ -0,0 +1,12 @@
+package com.team4.expo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpoApplication.class, args);
+    }
+}
