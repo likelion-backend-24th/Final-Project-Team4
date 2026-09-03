29f3b26725c1fe9d9dccd73aeb41dd1c1d7ff4df
김재혁 <kjhkmn80@gmail.com>
2026-09-03 16:03:35 +0900

    refactor: CORS를 Gateway로 일원화

--- backend/identity/src/main/java/com/team4/identity/config/SecurityConfig.java
+++ backend/identity/src/main/java/com/team4/identity/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.team4.identity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +14,6 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
