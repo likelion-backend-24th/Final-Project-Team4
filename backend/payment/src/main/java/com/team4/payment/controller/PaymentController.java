66c6129b5108a184686b8b653218ecbac0f2f587
김재혁 <kjhkmn80@gmail.com>
2026-09-03 21:49:39 +0900

    feat: expo 라우팅 수정, payment CORS 어노테이션 제거

--- backend/payment/src/main/java/com/team4/payment/controller/PaymentController.java
+++ backend/payment/src/main/java/com/team4/payment/controller/PaymentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/exhibitor/payments")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class PaymentController {
     private final PaymentService paymentService;
 
