4650592148995d46f27f0786c93d4a9daee48ae5
김재혁 <kjhkmn80@gmail.com>
2026-09-14 17:52:34 +0900

    fix: SecurityConfig 순환참조 수정

--- backend/identity/src/main/java/com/team4/identity/config/SecurityConfig.java
+++ backend/identity/src/main/java/com/team4/identity/config/SecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,10 +32,5 @@ public class SecurityConfig {
                         .successHandler(oAuth2SuccessHandler)
                 );
         return http.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
     }
 }
