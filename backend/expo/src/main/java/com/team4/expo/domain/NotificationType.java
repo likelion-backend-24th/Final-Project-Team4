3c16bf9a714ffc356d87eddf8107e5555cdb2e79
김재혁 <kjhkmn80@gmail.com>
2026-09-17 21:43:10 +0900

    feat: 박람회 일정 변경 알림 유형 추가

--- backend/expo/src/main/java/com/team4/expo/domain/NotificationType.java
+++ backend/expo/src/main/java/com/team4/expo/domain/NotificationType.java
@@ -5,5 +5,7 @@ public enum NotificationType {
     BOOTH_APPROVED,         // (참가업체) 부스 신청 승인
     BOOTH_REJECTED,         // (참가업체) 부스 신청 반려
     CONSULTATION_RECEIVED,  // (참가업체) 신규 상담 접수
-    CONSULTATION_APPROVED   // (고객) 상담 신청 확정(승인)
+    CONSULTATION_APPROVED,  // (고객) 상담 신청 확정(승인)
+    EXPO_TICKET_CANCELLED,  // (고객) 박람회 개최 기간 변경으로 기존 QR(방문예약)이 취소됨
+    EXPO_SCHEDULE_CHANGED   // (고객) 박람회 개최 기간이 변경됐지만 기존 QR은 그대로 유효함
 }
